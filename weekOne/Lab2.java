2b7e0cc4e89263b5e67c2022b715fbfe8464bf07
seejake23 <oceansseasons@gmail.com>
2023-04-03 15:33:38 -0400

    finished each task in the Lab

--- weekOne/Lab2.java
+++ weekOne/Lab2.java
@@ -1,16 +1,18 @@
 package J03.weekOne;
 
-import java.util.Scanner;
+import java.util.*;
 import java.lang.Math;
 import java.text.DecimalFormat;
 
 public class Lab2 {
     public static void main(String[] args) {
-        calculateCircle();
-        calculateTriangle();
-        oddOrEven();
-        intToDouble();
-        doubleToInt();
+        // calculateCircle();
+        // calculateTriangle();
+        // oddOrEven();
+        // intToDouble();
+        letterToAscii();
+        // doubleToInt();
+        // leapYear();
 
     }
 
@@ -44,8 +46,7 @@ public class Lab2 {
         if (userNumber % 2 == 0) {
             answer = "even";
             System.out.println("The number is even.");
-        } 
-        else {
+        } else {
             System.out.println("The number is odd.");
         }
     }
@@ -58,9 +59,14 @@ public class Lab2 {
         System.out.println("You entered " + wholeNumber + ", the new value is " + singleDecimal + ".");
     }
 
-    // public static void letterToAscii () {
-        
-    // }
+    public static void letterToAscii () {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a letter to have it converted to its acsii code: ");
+        String userInput = sc.nextLine(); 
+        char letter = userInput.charAt(0);
+        int ascii = (int) letter;
+        System.out.println("The acii code for "+ "'"+ letter+ "'" + " is " + ascii + ".");
+    }
 
     public static void doubleToInt() {
         Scanner sc = new Scanner(System.in);
@@ -71,5 +77,23 @@ public class Lab2 {
 
     }
 
-    
+    public static void leapYear() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What year would you like to check, to see if it was a leap year?: ");
+        int year = sc.nextInt();
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println("The year is a leap year (it has 366 days).");
+                } else {
+                    System.out.println("The year is not a leap year (it has 365 days).");
+                }
+            } else {
+                System.out.println("The year is not a leap year (it has 365 days).");
+            }
+        } else {
+            System.out.println("The year is not a leap year (it has 365 days).");
+        }
+    }
+
 }
