31c55f3126075cbd803e4e096d7055d1a5894935
seejake23 <oceansseasons@gmail.com>
2023-04-11 11:58:02 -0400

    Lab 4 completed

---
+++ weekTwo/Labs/Lab4/Q5/App.java
@@ -0,0 +1,12 @@
+package J03.weekTwo.Labs.Lab4.Q5;
+
+public class App {
+    public static void main(String[] args) {
+        
+        ConsoleIO io = new ConsoleIO();
+
+        //pass the word you want multiplied and how many times
+        io.repeatedString("hi", 6);
+    }
+
+}
